95a11f01cdafdd046504c8e5a01493a2b54fcac2
h4g0 <afonsohugo24@gmail.com>
2025-01-26 19:37:22 +0000

    updated dataset

--- stats/generate_stats.py
+++ stats/generate_stats.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from vega_lite import get_count_col
+from vega_lite import get_count_col, get_phase_domain_count, get_phase_domain_count_stacked
 
 graph_folder = "generated_graphs"
 
@@ -10,21 +10,81 @@ def write_to_file(contents,filename):
 def stats_column_count(col,df,max=-1):
     write_to_file(get_count_col(col,df[col],max),f"{graph_folder}/{col}.json")
 
+
+def transform_phase(phase):
+  
+  def helper(phase):
+    if phase == "all":
+      return ["cd","ci","cdev","ct","co","cm","cf"]
+  
+    else: 
+      return phase.split(",")
+    
+  phases = [x.upper() for x in helper(phase)]
+
+  return phases
+
+def transform_stats_phase(phase):
+  new_phase = list()
+
+  for p in phase:
+    for i in transform_phase(p):
+      new_phase.append(i) 
+
+  return new_phase
+
+
+def transform_stats_phase_domain(domain,phase):
+  new_phase_domain = list()
+
+  for p in range(len(domain)):
+    for i in transform_phase(phase[p]):
+      new_phase_domain.append((i,domain[p])) 
+
+  return new_phase_domain
+
+def combs_mde(df):
+  combs = list()
+
+  for e in range(1,len(df)):
+    comb = list()
+    if df["t2m"][e]:
+      comb.append("T2M")
+    if df["m2m"][e]:
+      comb.append("M2M")
+    if df["restrictions"][e]:
+      comb.append("OCL") 
+    if df["m2t"][e]:
+      comb.append("M2T")
+
+    combs.append(" and ".join(comb))
+  
+  return combs
+
 def get_stats(df):
-  cols = ["year", "domain", "phase", "validation", "type", "goal", "m2m", "m2t", "t2m", "restrictions","year"]
+  cols = ["year", "domain",  "validation", "type", "goal", "m2m", "m2t", "t2m", "restrictions","year"]
 
   for col in cols:
     stats_column_count(col,df)
 
+  
+  
   domain = df["domain"]
   phase = df["phase"]
+
+  transformed_phase = transform_stats_phase(phase)
+  write_to_file(get_count_col("phase",transformed_phase),f"{graph_folder}/phase.json")
 
   print(len(domain))
   print(len(phase))
 
-  phase_domain = [f"{domain[i]}, {phase[i]}" for i in range(len(domain))]
+  phase_domain = transform_stats_phase_domain(domain,phase)
 
-  write_to_file(get_count_col("phase_domain",phase_domain,5),f"{graph_folder}/phase_domain.json")
+  write_to_file(get_phase_domain_count_stacked(phase_domain), f"{graph_folder}/phase_domain.json")
+
+  combsmede = combs_mde(df)
+
+  write_to_file(get_count_col("combs mde",combsmede), f"{graph_folder}/combsmde.json")
 
 def main():
 
--- stats/vega_lite.py
+++ stats/vega_lite.py
@@ -25,25 +25,137 @@ def get_count_col(col_name,cols,max=-1):
 
 
     template =  '''{
-  
-  "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-  "description": "A bar chart with highlighting on hover and selecting on click. (Inspired by Tableau's interaction style.)",
-  "data": {
-    "values": [
-          (values)
-        ]
-  },
-  "mark":  {"type": "bar", "size": 20},
-  "encoding": {
-    "x": {"field": "(col_name)", "type": "ordinal","scale": {"padding": 5}, "sort": "-y"},
-    "y": {"field": "count", "type": "quantitative", "stack": false}
-  }
-  
-}
+      
+      "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+      "description": "A bar chart with highlighting on hover and selecting on click. (Inspired by Tableau's interaction style.)",
+      "data": {
+        "values": [
+              (values)
+            ]
+      },
+      "mark":  {"type": "bar", "size": 20},
+      "encoding": {
+        "x": {"field": "(col_name)", "type": "ordinal","scale": {"padding": 5}, "sort": "-y"},
+        "y": {"field": "count", "type": "quantitative", "stack": false}
+      }
+      
+    }
 
-'''
+    '''
 
     template = template.replace("(values)",values)
     template = template.replace("(col_name)",col_name)
 
+    return template
+
+
+
+def get_phase_domain_count(phase_domain):
+    
+    values = list()
+    count = dict()
+
+    for pd in phase_domain:
+      new_count = count.get(pd,0) + 1
+      count.update({pd: new_count})
+
+
+    for item in count.items():
+        pd, count_item = item
+        p,d = pd
+        
+        values.append(f"{{\"domain\":\"{d}\", \"phase\":\"{p}\", \"value\":{count_item}}}")
+
+    values = ",\n".join(values)
+    
+
+    template = '''{
+      "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+      "data": {
+        "values": [
+          (values)
+        ]
+      },
+      "mark": "bar",
+      "encoding": {
+        "x": {"field": "domain"},
+        "y": {"field": "value", "type": "quantitative"},
+        "xOffset": {"field": "phase"},
+        "color": {"field": "phase"}
+      }
+    }'''
+    
+    template = template.replace("(values)",values)
+    
+    return template
+
+
+def get_phase_domain_count_stacked(phase_domain):
+    
+    values = list()
+    count = dict()
+
+    for pd in phase_domain:
+      new_count = count.get(pd,0) + 1
+      count.update({pd: new_count})
+
+
+    for item in count.items():
+        pd, count_item = item
+        p,d = pd
+        
+        values.append(f"{{\"domain\":\"{d}\", \"phase\":\"{p}\", \"value\":{count_item}}}")
+
+    values = ",\n".join(values)
+    
+    template = '''{
+    "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+    "data": {
+      "values": [
+        {"domain":"Cloud", "phase":"CD", "value":13},
+        {"domain":"Cloud", "phase":"CI", "value":8},
+        {"domain":"Generic", "phase":"CI", "value":5},
+        {"domain":"Generic", "phase":"CD", "value":5},
+        {"domain":"Safety-Crytical Systems", "phase":"CT", "value":1},
+        {"domain":"Safety-Crytical Systems", "phase":"CI", "value":1},
+        {"domain":"Safety-Crytical Systems", "phase":"CD", "value":1},
+        {"domain":"IoT", "phase":"CI", "value":2},
+        {"domain":"IoT", "phase":"CD", "value":4},
+        {"domain":"Generic", "phase":"CDEV", "value":3},
+        {"domain":"Generic", "phase":"CT", "value":3},
+        {"domain":"Generic", "phase":"CO", "value":4},
+        {"domain":"Generic", "phase":"CM", "value":3},
+        {"domain":"Generic", "phase":"CF", "value":3},
+        {"domain":"Data Science", "phase":"CD", "value":1},
+        {"domain":"Data Science", "phase":"CI", "value":1},
+        {"domain":"Data Science", "phase":"CDEV", "value":1},
+        {"domain":"Data Science", "phase":"CT", "value":1},
+        {"domain":"Data Science", "phase":"CO", "value":1},
+        {"domain":"Data Science", "phase":"CM", "value":1},
+        {"domain":"Data Science", "phase":"CF", "value":1},
+        {"domain":"Security", "phase":"CD", "value":2},
+        {"domain":"Security", "phase":"CI", "value":2},
+        {"domain":"Security", "phase":"CDEV", "value":2},
+        {"domain":"Security", "phase":"CT", "value":2},
+        {"domain":"Security", "phase":"CO", "value":2},
+        {"domain":"Security", "phase":"CM", "value":2},
+        {"domain":"Security", "phase":"CF", "value":2},
+        {"domain":"Cyber-Physical Systems", "phase":"CDEV", "value":1},
+        {"domain":"Cyber-Physical Systems", "phase":"CT", "value":1},
+        {"domain":"Cyber-Physical Systems", "phase":"CM", "value":1}
+      ]
+    },
+    "transform": [
+      {"filter": "datum.value > 0"}
+    ],
+    "mark": "bar",
+    "encoding": {
+      "x": {"field": "phase", "type": "nominal", "axis": {"title": "phase"}},
+      "y": {"field": "value", "type": "quantitative", "axis": {"title": "value"}},
+      "color": {"field": "domain", "type": "nominal", "legend": {"title": "domain"}}
+    }
+    }'''
+    
+    template = template.replace("(values)",values)
+    
     return template
